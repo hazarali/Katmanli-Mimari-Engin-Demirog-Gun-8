de4001485dd0ad83f19a262b3807e26c782624e4
hazarali <alihazar@outlook.com.tr>
2023-01-24 17:56:09 +0300

    Katmanlı Proje
    Engin Demiroğ 8. gün dersi

---
+++ KatmanlıProje/Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+
+        List<Product> GetAllCategory(int id);   // kategoriye göre filtreleyeceğimiz için hangi kategori olacağını almamamız gerek
+
+        List<Product> GetByUnitPrices(decimal min, decimal max);
+
+    }
+}
---
+++ KatmanlıProje/ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductManager productManager = new ProductManager(new EFProductDal());
+
+            foreach (var item in productManager.GetByUnitPrices(40,100))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.ReadLine();
+
+          
+        }
+    }
+}
